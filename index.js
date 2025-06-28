f5301d433124dd348ca8857dee283cbe85b5fbe1
TJ-Projects3 <tjprojects3@gmail.com>
2025-06-28 14:14:26 -0400

    Added renderList functiomn, optimized with DOM caching, and added small CSS

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 let userList = []
-let inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
-let listEl = document.getElementById("list-el")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const listEl = document.getElementById("list-el")
 
 inputBtn.addEventListener("click", function() {
-    userList.push(listEl.value)
+    userList.push(inputEl.value)
+    inputEl.value = ""
+    renderList()
 })
+
+function renderList() {
+    let inputList = ""
+    for (let i = 0; i < userList.length; i++) {
+        inputList += `<li>
+                        <a href="${userList[i]}" target="_blank">
+                            ${userList[i]}
+                        </a>
+                     </li>`
+    }
+    listEl.innerHTML = inputList
+}
