b9b78f8bbdcc5265a3d247b996d9e050148ee079
TJ-Projects3 <tjprojects3@gmail.com>
2025-07-05 10:37:32 -0400

    Local storage functions and delete button CSS

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ let userList = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const listEl = document.getElementById("list-el")
+const deleteBtn = document.getElementById("delete-btn")
+const listFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) // Parses stringed list out to become a regular list
+
+if (leadsFromLocalStorage) {
+    userList = listFromLocalStorage
+    renderList()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderList()
+})
 
 inputBtn.addEventListener("click", function() {
     userList.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) // Makes the list a string
     renderList()
 })
 
